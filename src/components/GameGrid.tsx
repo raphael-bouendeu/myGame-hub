d679bf7ac46de9ad40e92ed3c51f5d581bbdd131
bouendeu <raphael.bouendeu@itbcafrica.com>
2023-04-26 11:55:31 +0200

    Filter  games by genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,9 +3,14 @@ import useGames from "../hooks/useGames"
 import GameCard from "./GameCard"
 import GameCardSkeleton from "./GameCardSkeleton"
 import GameCardContainer from "./GameCardContainer"
+import { Genre } from "../hooks/useGenres"
 
-const GameGrid = () => {
-  const { error, data, isLooading } = useGames()
+interface Props {
+  selectedGenre: Genre | null
+}
+
+const GameGrid = ({ selectedGenre }: Props) => {
+  const { error, data, isLooading } = useGames(selectedGenre)
   const skeletons = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   ]
@@ -19,15 +24,15 @@ const GameGrid = () => {
       >
         {isLooading &&
           skeletons.map((skeleton) => (
-            <GameCardContainer>
+            <GameCardContainer key={skeleton}>
               {" "}
-              <GameCardSkeleton key={skeleton} />
+              <GameCardSkeleton />
             </GameCardContainer>
           ))}
         {data.map((game) => (
-          <GameCardContainer>
+          <GameCardContainer key={game.id}>
             {" "}
-            <GameCard key={game.id} game={game} />{" "}
+            <GameCard game={game} />{" "}
           </GameCardContainer>
         ))}
       </SimpleGrid>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,21 @@
 import React from "react"
-import useGenres from "../hooks/useGenres"
-import { List, ListItem, HStack, Image, Text, Spinner } from "@chakra-ui/react"
+import useGenres, { Genre } from "../hooks/useGenres"
+import {
+  List,
+  ListItem,
+  HStack,
+  Image,
+  Text,
+  Spinner,
+  Button,
+} from "@chakra-ui/react"
 import getCroppedImageUrl from "../services/image-url"
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (genre: Genre) => void
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
   const { data, isLooading, error } = useGenres()
   if (error) return null
   if (isLooading) return <Spinner />
@@ -17,7 +29,13 @@ const GenreList = () => {
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Text fontSize="lg">{genre.name}</Text>
+            <Button
+              onClick={() => onSelectGenre(genre)}
+              fontSize="lg"
+              variant="link"
+            >
+              {genre.name}
+            </Button>
           </HStack>
         </ListItem>
       ))}
