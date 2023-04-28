32747d47532ef168b63b384f955160fb3feec29d
bouendeu <raphael.bouendeu@itbcafrica.com>
2023-04-28 16:27:59 +0200

    Implement Searching

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,14 @@ import logo from "../assets/logo.png"
 import ColorModeSwitch from "./ColorModeSwitch"
 import SearchInput from "./SearchInput"
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void
+}
+const NavBar = ({ onSearch }: Props) => {
   return (
-    <HStack padding="10px">
+    <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   )
